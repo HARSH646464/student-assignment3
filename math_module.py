f569713c967f79f6f112c92e9acda48a34612ea6
HARSH646464 <harshtomar4081@gmail.com>
2025-08-25 18:52:16 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,10 @@
+import math
+def module():
+    number=int(input('Enter the number:'))
+    square_root=math.sqrt(number)
+    print('Square root:',square_root)
+    sin=math.sin(number)
+    print('sine:',sin)
+    logarithim=math.log(number)
+    print('Logarithim:',logarithim )
+module()
